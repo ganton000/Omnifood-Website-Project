2d4439f3b06637521a4b78c34bd494ed460767ce
George Anton <SendittoGeorgeAnton@gmail.com>
2022-05-15 22:57:08 -0400

    Added JS script and optimized photos

---
+++ js/script.js
@@ -0,0 +1,93 @@
+
+//Change copyright year
+const yearEl = document.querySelector(".year")
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+//Toggle mobile navigation
+const btnNavEl = document.querySelector(".btn-mobile-nav")
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener('click', () => {
+	headerEl.classList.toggle('nav-open')
+});
+
+//Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(link =>
+	link.addEventListener('click', (e) => {
+		e.preventDefault();
+		//get href attribute
+		const href = link.getAttribute("href");
+
+		//Scroll back to top
+		if (href === "#") {
+
+			window.scrollTo({
+					top: 0,
+					behavior: "smooth"
+				})
+		}
+
+		//Scroll to other links
+		if (href !== "#" && href.startsWith("#")) {
+				const sectionEl = document.querySelector(href);
+
+				sectionEl.scrollIntoView({behavior: "smooth"})
+		}
+
+		//Close mobile navigation
+
+		if (link.classList.contains('main-nav-link')) {
+			headerEl.classList.toggle('nav-open')
+		};
+	})
+)
+
+//Sticky navigation
+
+const sectionHeroEl = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver((entries) => {
+	const ent = entries[0];
+	if (!ent.isIntersecting) {
+		document.body.classList.add('sticky');
+	}
+
+	if (ent.isIntersecting) {
+		document.body.classList.remove('sticky');
+	}
+
+}, {
+	//observed in viewport
+	root: null,
+	//event triggers as soon as 0% of hero section is completely outside of viewport
+	threshold: 0,
+	//height navbar = 8rem = 80px; compensates to not overlap featured-in-section
+	rootMargin: "-80px"
+})
+
+obs.observe(sectionHeroEl);
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+	var flex = document.createElement("div");
+	flex.style.display = "flex";
+	flex.style.flexDirection = "column";
+	flex.style.rowGap = "1px";
+
+	flex.appendChild(document.createElement("div"));
+	flex.appendChild(document.createElement("div"));
+
+	document.body.appendChild(flex);
+	var isSupported = flex.scrollHeight === 1;
+	flex.parentNode.removeChild(flex);
+	console.log(isSupported);
+
+	if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  }
+  checkFlexGap();
+
+  // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
